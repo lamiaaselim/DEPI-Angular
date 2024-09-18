f623fe5e2c03b1c21606f4464109e3f1aba32766
Lamiaa Selim <lamiaaselim1896@gmail.com>
2024-09-18 20:08:50 +0300

    upload day5-2

--- secondapp/src/app/app-routing.module.ts
+++ secondapp/src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
-  { path: '/home', component: HomeComponent },
-  { path: '/employee-list', component: EmployeeListComponent },
-  { path: '/employee-detials', component: EmployeeDetailsComponent },
-  { path: '/login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'employee-list', component: EmployeeListComponent },
+  { path: 'employee-detials', component: EmployeeDetailsComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
