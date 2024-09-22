cb0292b3c05b808d4759afe86e81860902a6bf9e
Lamiaa Selim <lamiaaselim1896@gmail.com>
2024-09-22 21:13:05 +0300

    upload day7-1

--- secondapp/src/app/components/reactiv-forms/reactiv-forms.component.ts
+++ secondapp/src/app/components/reactiv-forms/reactiv-forms.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactiv-forms',
   templateUrl: './reactiv-forms.component.html',
-  styleUrl: './reactiv-forms.component.scss'
+  styleUrl: './reactiv-forms.component.scss',
 })
 export class ReactivFormsComponent {
+  // registerForm = new FormGroup({
+  //   userName: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // })
+  registerForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: [''],
+      }),
+    });
+  }
 
+  getData() {
+    this.registerForm.patchValue({
+      userName: 'Lamiaa',
+      address: {
+        city: 'Egypt',
+        state: 'Giza',
+        postalCode: '1234',
+      },
+    });
+  }
 }
